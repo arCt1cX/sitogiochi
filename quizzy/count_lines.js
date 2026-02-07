b0ec8a7e4aee5dcdb5012e112578bfbf0e725da1
arCt1cX <92270123+arCt1cX@users.noreply.github.com>
2026-02-07 14:55:49 +0100

    english support

--- quizzy/count_lines.js
+++
@@ -1,9 +0,0 @@
-
-const fs = require('fs');
-const json = JSON.parse(fs.readFileSync('remaining_it.json', 'utf8'));
-
-Object.keys(json).forEach(key => {
-    const str = JSON.stringify(json[key], null, 2);
-    const lines = str.split('\n').length;
-    console.log(`${key}: ${lines} lines`);
-});
